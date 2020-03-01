201b869a605f9b510f5d3e7b4bdc7d97d695ac2b
Yegor Kozlovskiy <basterfly@gmail.com>
2020-03-01 17:44:37 +0200

    Was made a project for First App TableView by Senin course. Changed the branch Master to Developing

---
+++ SeninTableViewFirstApp/SeninTableViewFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeninTableViewFirstApp
+//
+//  Created by Yegor Kozlovskiy on 01.03.2020.
+//  Copyright © 2020 Yegor Kozlovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
