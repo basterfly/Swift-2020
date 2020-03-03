e7440105fc43045a9fdc361ba210bfd0c7d4c5a3
Yegor Kozlovskiy <basterfly@gmail.com>
2020-03-03 21:33:56 +0200

    added to ViewController: Button and TableView with Cell. Made property dataArray, connected tableView like IBOutlet, added dataSource and delegate for tableView, Cell ID = MyCell

--- SeninTableViewFirstApp/SeninTableViewFirstApp/ViewController.swift
+++ SeninTableViewFirstApp/SeninTableViewFirstApp/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var dataArray = ["Marina", "Masha", "Maria", "Larisa", "Valentina", "Lisa"]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = dataArray[indexPath.row]
+        return cell
+    }
+    
+    
+}
