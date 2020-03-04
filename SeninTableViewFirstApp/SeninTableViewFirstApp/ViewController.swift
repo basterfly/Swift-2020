48a9150275d6bbf79202eb2fdac854d3f42e0a50
Yegor Kozlovskiy <basterfly@gmail.com>
2020-03-04 13:40:49 +0200

    added methods to UITableViewDelegate and UITableViewDataSource for deleting rows and data on it

--- SeninTableViewFirstApp/SeninTableViewFirstApp/ViewController.swift
+++ SeninTableViewFirstApp/SeninTableViewFirstApp/ViewController.swift
@@ -21,19 +21,29 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    } // возвращаем значение стиля для свайпа
+
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Delete"
+    } // Возвращаем текст который будет появлятся на свайпе когда мы хотим удалить объект
+
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        return dataArray.count //возвращаем кол-во строк в таблице
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-        cell.textLabel?.text = dataArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) // создаем ячейку
+        cell.textLabel?.text = dataArray[indexPath.row] //присваиваем значение ячейке
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        dataArray.remove(at: indexPath.row) //удаление данных
+        tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade) // удаление ячейки
+    } //в данном методе нам нужно произвести удаление выбранной ячейки и данных
 }
