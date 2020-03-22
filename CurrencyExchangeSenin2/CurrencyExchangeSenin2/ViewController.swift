5f0d442d7677d709d48a77e4bb2b19d2b2d9c8e2
Yegor Kozlovskiy <basterfly@gmail.com>
2020-03-22 13:52:55 +0200

    Made new project for task 2 Currency Exchange

---
+++ CurrencyExchangeSenin2/CurrencyExchangeSenin2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyExchangeSenin2
+//
+//  Created by Yegor Kozlovskiy on 1/14/20.
+//  Copyright © 2020 Yegor Kozlovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
