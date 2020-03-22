61d690195c198d93f7d7c4d6ce0ae3b0ea7892e5
Yegor Kozlovskiy <basterfly@gmail.com>
2020-03-22 13:59:39 +0200

    Added data.xml file from bank site. Added new file Model.swift.

---
+++ CurrencyExchangeSenin2/CurrencyExchangeSenin2/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  CurrencyExchangeSenin2
+//
+//  Created by Yegor Kozlovskiy on 22.03.2020.
+//  Copyright © 2020 Yegor Kozlovskiy. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
