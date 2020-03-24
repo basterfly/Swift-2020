070316e77aab8b62beff68e5d7093897e1cef9e3
Yegor Kozlovskiy <basterfly@gmail.com>
2020-03-24 16:27:03 +0200

    Video 17 XML Parsing. XML Parser.

--- CurrencyExchangeSenin2/CurrencyExchangeSenin2/AppDelegate.swift
+++ CurrencyExchangeSenin2/CurrencyExchangeSenin2/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         Model.shared.pathForXML
+        Model.shared.parseXML()
         
         return true
     }
--- CurrencyExchangeSenin2/CurrencyExchangeSenin2/Model.swift
+++ CurrencyExchangeSenin2/CurrencyExchangeSenin2/Model.swift
@@ -23,7 +23,7 @@ class Curruncy {
 
 }
 
-class Model: NSObject {
+class Model: NSObject, XMLParserDelegate {
     static let shared = Model() //sharedInstance - создаем синглтон класса модель, т.е. инстанс класса Модель. Будем обращаться к нему и с ним всегда работать, а не плодить новые, тем самым не расходуем память.
     var currencies: [Curruncy] = []
     
@@ -38,7 +38,7 @@ class Model: NSObject {
         return Bundle.main.path(forResource: "data", ofType: "xml")!
     }
     
-    var urlForXML: URL? {
+    var urlForXML: URL {
         return URL(fileURLWithPath: pathForXML)
     }
     
@@ -48,6 +48,52 @@ class Model: NSObject {
     }//func для загрузки xml
     //распарсить XML и положить его в currencies: [Curruncy], отправить уведомление приложению о том что данные обновились
     func parseXML() {
+        let parser = XMLParser(contentsOf: urlForXML)
+        //нужно парсеру назначить делегата и модель назначить соответствовать делегату XMLParserDelegate
+        parser?.delegate = self
+        parser?.parse()
         
+        print(currencies)
+    }
+    
+    var currentCurrancy: Curruncy?
+    
+    //вместо написания extensionа и реализации в нем методов дегата пишем тут
+    //данный метод ищет в XMLе данные с : <ИМЯ> (начало строки) и вносит их в словарь attributeDict
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        if elementName == "Valute" { //если элемент имеется то создадим какуюто валюту
+            currentCurrancy = Curruncy()
+        }
+    }
+    
+    var currentCaracters: String = ""
+       //этот метод вызывается когда в XML находятся цифры между началом и концом строки <NAME>098</NAME>
+       func parser(_ parser: XMLParser, foundCharacters string: String) {
+           currentCaracters = string
+       }
+    
+    //данный метод ищет в XMLе данные с : </ИМЯ> (конец строки) и вызывается когда находит их
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "NumCode" {
+            currentCurrancy?.NumCode = currentCaracters
+        }
+        if elementName == "CharCode" {
+            currentCurrancy?.CharCode = currentCaracters
+        }
+        if elementName == "Nominal" {
+            currentCurrancy?.Nominal = currentCaracters
+            currentCurrancy?.nominalDouble = Double(currentCaracters.replacingOccurrences(of: ",", with: ".")) // replacingOccurrences применяем для преобразования зяпятой в точку
+        }
+        if elementName == "Name" {
+            currentCurrancy?.Name = currentCaracters
+        }
+        if elementName == "Value" {
+            currentCurrancy?.Value = currentCaracters
+            currentCurrancy?.valueDouble = Double(currentCaracters.replacingOccurrences(of: ",", with: ".")) // replacingOccurrences применяем для преобразования зяпятой в точку
+        }
+        
+        if elementName == "Valute" { //если элемент закрылся в XML то мы его добавляем в массив
+            currencies.append(currentCurrancy!)
+        }
     }
 }
