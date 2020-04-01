6ae9bc4816a3f489240c808af2035092ac153624
Yegor Kozlovskiy <basterfly@gmail.com>
2020-04-01 17:36:15 +0300

    video 21 Prepare the view for downloading prices of currencies

--- CurrencyExchangeSenin2/CurrencyExchangeSenin2/CoursesController.swift
+++ CurrencyExchangeSenin2/CurrencyExchangeSenin2/CoursesController.swift
@@ -12,6 +12,9 @@ class CoursesController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        navigationItem.title = "Курсы валют на " + dateFormatter.string(from: Model.shared.currentDate)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
--- CurrencyExchangeSenin2/CurrencyExchangeSenin2/Model.swift
+++ CurrencyExchangeSenin2/CurrencyExchangeSenin2/Model.swift
@@ -26,6 +26,7 @@ class Curruncy {
 class Model: NSObject, XMLParserDelegate {
     static let shared = Model() //sharedInstance - создаем синглтон класса модель, т.е. инстанс класса Модель. Будем обращаться к нему и с ним всегда работать, а не плодить новые, тем самым не расходуем память.
     var currencies: [Curruncy] = []
+    var currentDate: Date = Date() //переменная с текущей датой но которую мы сможем менять
     
     //если файл будет загружен то будем брать данные из него!, если файл не загружен то данные будем брать из файла data.xml
     var pathForXML: String {
@@ -87,6 +88,15 @@ class Model: NSObject, XMLParserDelegate {
     //вместо написания extensionа и реализации в нем методов дегата пишем тут
     //данный метод ищет в XMLе данные с : <ИМЯ> (начало строки) и вносит их в словарь attributeDict
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        if elementName == "ValCurs" {
+            if let currentDateString = attributeDict["Date"] {
+                //преобразуем дату из строки в дату
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "dd.MM.yyyy"
+                currentDate = dateFormatter.date(from: currentDateString)!
+            }
+        }
+        
         if elementName == "Valute" { //если элемент имеется то создадим какуюто валюту
             currentCurrancy = Curruncy()
         }
@@ -100,6 +110,7 @@ class Model: NSObject, XMLParserDelegate {
     
     //данный метод ищет в XMLе данные с : </ИМЯ> (конец строки) и вызывается когда находит их
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
         if elementName == "NumCode" {
             currentCurrancy?.NumCode = currentCaracters
         }
